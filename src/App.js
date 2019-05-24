dea4a42e5002e6caaef3d9a89645743b6b4ee7a9
Drew Hemsley <drew@grow.com>
2019-05-23 20:57:19 -0600

    just learning react - you know me

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+
+class Login extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      username: '',
+      password: ''
+    }
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleInputChange(event) {
+    const key = event.target.className;
+    const value = event.target.value;
+
+    this.setState({ [key]: value })
+  }
+
+  handleClick(event){
+    const username = this.state.username;
+    const password = this.state.password;
+
+    if (!username) {
+      return alert('please enter a username')
+    }
+
+    if (password.length < 8) {
+      return alert('password must be at least 8 characters')
+    }
+
+    console.log('login success!!!')
+  }
+
+  render(){
+    return (
+      <div className="login-modal">
+        <input value={this.state.username} onChange={this.handleInputChange} className="username" type="text" placeholder="username"/>
+        <input value={this.state.password} onChange={this.handleInputChange} className="password" type="password" placeholder="password"/>
+        <div className="submit" onClick={this.handleClick}>
+          <span>Login</span>
+        </div>
+      </div>
+    )
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="wrapper">
+    <Login />
+  </div>
   );
 }
 
